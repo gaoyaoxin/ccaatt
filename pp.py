2ab40ff69805a225a991920c5c8e0f07d31e731d
高垚鑫 <gaoyaoxin@bfsu.edu.cn>
2018-12-22 21:03:52 +0800

    Update pp.py

--- pp.py
+++ pp.py
@@ -78,7 +78,11 @@ while 1:
     except:
         print('请输入一个整数')
 
-alphabet =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+alphabet =['a','b','c','d','e','f','g','h','i','j','k','l','m',
+           'n','o','p','q','r','s','t','u','v','w','x','y','z',
+           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+           'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+           'U', 'V', 'W', 'X', 'Y', 'Z']
 
 for i in range(1, numOfLayers):
     print((numOfLayers-i)*' ' + (alphabet[i-1]+' ')*(i-1) + (alphabet[i-1]+' ') + (numOfLayers-i)*' ')
